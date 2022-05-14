4bf8f19a9fdb86076a887b8f2b5b66121da1fc43
Dang Hoang Khang <dhkhang1709@gmail.com>
2022-05-14 19:22:16 +0700

    Add project files.

---
+++ AwaitAsync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AwaitAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
